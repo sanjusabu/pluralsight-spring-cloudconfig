c47a07e86b4003606526cd8e52092c22d519c685
sanju <sanjusabu066@gmail.com>
2024-08-02 04:38:05 +0530

    hello

---
+++ src/test/java/com/pluralsight/configservergit/ConfigserverGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.configservergit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
